c6ca13347c629f8c59807811875f27753da98769
Vidhaya Datta Reddy Metta <115148597+VidhuDatta@users.noreply.github.com>
2025-04-20 14:56:50 -0700

    Delete capstone_proj/template_proj directory

--- capstone_proj/template_proj/cfg/constraints.tcl
+++
@@ -1,9 +0,0 @@
-# constraints.tcl
-#
-# This file is where design timing constraints are defined for Genus and Innovus.
-# Many constraints can be written directly into the Hammer config files. However, 
-# you may manually define constraints here as well.
-#
-
-# TODO: add constraints here!
-
